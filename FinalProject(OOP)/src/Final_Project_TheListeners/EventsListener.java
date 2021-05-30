e03c099ee0fd6bdb8046f3600f86f5cacb9b24a3
eladsabag <68230346+eladsabag@users.noreply.github.com>
2021-05-30 12:05:29 +0300

    Add files via upload

---
+++ FinalProject(OOP)/src/Final_Project_TheListeners/EventsListener.java
@@ -0,0 +1,14 @@
+package Final_Project_TheListeners;
+
+public interface EventsListener {
+
+	void fireActionCompleted();
+
+	void fireActionFailed();
+
+	void fireCountryEmpty();
+
+	void fireCountrySame();
+
+	void fireEndOlympicsFailed();
+}
